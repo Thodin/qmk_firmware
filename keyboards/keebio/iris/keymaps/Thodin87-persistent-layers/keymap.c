7b1cf3bf695ff8cb986c21a533ea88219b696d87
Max Meindl <max_meindl@gmx.de>
2023-06-29 09:02:36 +0200

    numbers on adjust, allow lower during adjust

--- keyboards/keebio/iris/keymaps/Thodin87-persistent-layers/keymap.c
+++ keyboards/keebio/iris/keymaps/Thodin87-persistent-layers/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,          _______, KC_CIRC, KC_LCBR, KC_RCBR, KC_AMPR, KC_ASTR, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, _______,                   _______, _______, _______
+                                    _______, _______, _______,                   _______, TG(ADJUST), _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
   
@@ -60,13 +60,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_TOG, LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5),             KC_COMM, KC_7,    KC_8,    KC_9,    RALT(KC_Q), KC_DEL,
+     RGB_TOG, LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5),             KC_COMM, KC_F7,   KC_F8,   KC_F9,   RALT(KC_Q), KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_MOD, KC_MPLY, KC_MNXT, RALT(KC_S),KC_VOLU, _______,                          KC_DOT,  KC_4,    KC_5,    KC_6,    RALT(KC_P), KC_ENT,
+     RGB_MOD, KC_MPLY, KC_MNXT, RALT(KC_S),KC_VOLU, _______,                          KC_DOT,  KC_F4,   KC_F5,   KC_F6,   RALT(KC_P), KC_ENT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_MSTP, KC_MPRV, KC_MUTE, KC_VOLD, _______, _______,          _______, KC_0,    KC_1,    KC_2,    KC_3,    RALT(KC_Y), _______,
+     _______, KC_MSTP, KC_MPRV, KC_MUTE, KC_VOLD, _______, _______,          _______, KC_0,    KC_F1,   KC_F2,   KC_F3,   RALT(KC_Y), _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, _______,                   _______, _______, _______
+                                    _______, TG(ADJUST), _______,                _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -74,26 +74,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     QK_RBT,  _______, LGUI(KC_7), LGUI(KC_8), LGUI(KC_9), _______,                   KC_F12,  KC_F7,   KC_F8,   KC_F9,   _______, _______,
+     QK_RBT,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, LGUI(KC_4), LGUI(KC_5), LGUI(KC_6), _______,                   KC_F11,  KC_F4,   KC_F5,   KC_F6,   _______, KC_PSCR,
+     KC_PSCR, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), _______, _______, _______, KC_F10,  KC_F1,   KC_F2,   KC_F3,   _______, _______,
+     _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, _______,                   _______, _______, _______
+                                    _______, LOWER, _______,                 _______, TO(COLEMAK), _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   )
 };
 
+bool was_in_adjust = false;
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case LOWER:
       if (record->event.pressed) {
+        // Allow to use the LOWER layer by holding the key down while
+        // in ADJUST. When releasing LOWER, return to ADJUST rather than
+        // the default layer.
+        if (layer_state_is(_ADJUST)) {
+          was_in_adjust = true;
+          layer_off(_ADJUST);
+        }
+        else {
+          was_in_adjust = false;
+        }
         layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        // update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        if (was_in_adjust) {
+          layer_on(_ADJUST);
+        }
+        // update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
       break;
@@ -101,10 +115,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        // update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        // update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
       break;
